d7c05765d136573f9ce2666d40ef2a772ed4679a
Slimwolff <ptudoandtodos@gmail.com>
2022-07-28 15:58:25 -0300

    improve function that colorize elements

--- init.js
+++ init.js
@@ -1,6 +1,6 @@
 let gallery = document.querySelector('.gallery-wrapper'), navHead = document.querySelectorAll('.header-nav-inner');
 
-colors = ['e8f3fe','cce4fd','a4cefc','77b6fb','4b9cf9','2382f7','057af0','196bde','1557c0'];
+const COLORS = ['e8f3fe','cce4fd','a4cefc','77b6fb','4b9cf9','2382f7','057af0','196bde','1557c0'];
 
 
 function colorGallery(){
@@ -19,8 +19,30 @@ function colorHeadNav() {
     }
 }
 
+
+function colorObjects(arrayObj){
+    let c = 0;
+    var obj;
+    for(let i=0; i<arrayObj.length; i++) {
+        if (arrayObj[i] instanceof HTMLElement) {
+            obj = arrayObj[i].children;
+        } else if (arrayObj[i] instanceof HTMLCollection || arrayObj[i] instanceof NodeList) {
+            obj = arrayObj[i];
+        }
+        let objLength = obj.length;
+        for(let a=0; a<objLength; a++) {
+            if(c==COLORS.length-1) c=0;
+            obj[a].style.backgroundColor = "#"+COLORS[c];
+            c++;
+        }
+    }
+}
+
+
 window.onload = function() {
-    colorGallery();
-    // colorHeadNav()
+
+    colorObjects([gallery,navHead]);
+    //colorGallery();
+    //colorHeadNav();
 }
 
