a4bfdae96e801862945122843305d811e1b22801
Slimwolff <ptudoandtodos@gmail.com>
2022-08-15 16:27:15 -0300

    fix footer layout - improve coloring objects function

--- init.js
+++ init.js
@@ -1,6 +1,6 @@
-let gallery = document.querySelector('.gallery-wrapper'), footer = document.querySelectorAll('.index-gallery-content');
+let gallery = document.querySelector('.gallery-wrapper'), footer = document.querySelector('.footer-gallery');
 
-const COLORS = ['e8f3fe','cce4fd','a4cefc','77b6fb','4b9cf9','2382f7','057af0','196bde','1557c0'];
+const COLORS = ['#e8f3fe','#cce4fd','#a4cefc','#77b6fb','#4b9cf9','#2382f7','#057af0','#196bde','#1557c0'];
 
 
 
@@ -24,19 +24,23 @@ function colorHeadNav() {
 
 
 
-function colorObjects(arrayObj){
+function colorObjects(array){
     let c = 0;
     var obj;
-    for(let i=0; i<arrayObj.length; i++) {
-        if (arrayObj[i] instanceof HTMLElement) {
-            obj = arrayObj[i].children;
-        } else if (arrayObj[i] instanceof HTMLCollection || arrayObj[i] instanceof NodeList) {
-            obj = arrayObj[i];
+
+    for(let i=0; i<array.length; i++) {
+
+        if (array[i].children instanceof HTMLCollection) {
+            obj = array[i].children;
+        } else {
+            obj = array[i];
         }
+
         let objLength = obj.length;
+
         for(let a=0; a<objLength; a++) {
             if(c==COLORS.length-1) c=0;
-            obj[a].style.backgroundColor = "#"+COLORS[c];
+            obj[a].style.backgroundColor = COLORS[c];
             c++;
         }
     }
